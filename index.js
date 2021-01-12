719d4c04daa299dbf5d509e91fbb13e8a415b4fd
Pradas137 <pradas137@gmail.com>
2021-01-12 20:33:20 +0100

    nueva

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const express = require('express')
+var bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
+app.use(bodyParser.urlencoded({ extended: false }))
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('<form  method="post">\
+  Nom: <input type="text" name="nom">\
+  <input type="submit" value="Enviar">\
+  </form>')
 })
 
-app.post('/adeu', (req, res)) => {
-   res.send('Goodbye Hell!')
-}
+app.post("/", function(req, res) {
+    var nom = req.body.nom
+    res.send("Hello "+nom);
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
